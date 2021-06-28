0184901e509c42dc1fe713a7e39950268d230946
prashant indurkar <prashantri2000@gmail.com>
2021-06-28 16:49:01 +0530

    completed project

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hi");
+const myClock = setInterval( ()=> {
+    myTime();
+}, 1000);
+
+function myTime() {
+    let date = new Date();
+    document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+}
